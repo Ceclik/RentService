a8c4b486dbb94d85cda3dbefa49921d494f6c878
Ceclik <yura11122345@gmail.com>
2025-03-03 14:28:46 +0300

    Added db connection and user model

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import * as process from 'node:process';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { User } from './users/users.model';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+    }),
+    UsersModule,
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '44587001',
+      database: 'rentService',
+      models: [User],
+      autoLoadModels: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/users/users.model.ts
@@ -0,0 +1,38 @@
+import { Column, DataType, Model, Table } from 'sequelize-typescript';
+
+interface UserCreationAttrs {
+  email: string;
+  password: string;
+}
+
+@Table({ tableName: 'users' })
+export class User extends Model<User, UserCreationAttrs> {
+  @Column({
+    type: DataType.INTEGER,
+    unique: true,
+    autoIncrement: true,
+    primaryKey: true,
+  })
+  id: number;
+
+  @Column({ type: DataType.STRING, unique: true, allowNull: false })
+  email: string;
+
+  @Column({ type: DataType.STRING, allowNull: false })
+  password_hash: string;
+
+  @Column({ type: DataType.BOOLEAN, allowNull: false, defaultValue: false })
+  banned: boolean;
+
+  @Column({ type: DataType.STRING })
+  ban_reason: string;
+
+  @Column({ type: DataType.STRING })
+  oauth_provider: string;
+
+  @Column({ type: DataType.STRING })
+  oauth_id: string;
+
+  @Column({ type: DataType.STRING, allowNull: false, defaultValue: 'ADMIN' })
+  role: string;
+}
