c3086e13b72c782611a21cacbdf0c756d18f3e08
Ceclik <yura11122345@gmail.com>
2025-03-03 15:26:24 +0300

    Added user creating and getting

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,23 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
-import * as process from 'node:process';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { User } from './users/users.model';
+import * as process from 'node:process';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: `.${process.env.NODE_ENV}.env`,
+      envFilePath: `.env`,
     }),
     UsersModule,
     SequelizeModule.forRoot({
       dialect: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '44587001',
-      database: 'rentService',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
       models: [User],
       autoLoadModels: true,
     }),
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private userService: UsersService) {}
+
+  @Post('/add')
+  create(@Body() dto: CreateUserDto) {
+    return this.userService.createUser(dto);
+  }
+
+  @Get('/all')
+  getAll() {
+    return this.userService.getAllUsers();
+  }
+}
